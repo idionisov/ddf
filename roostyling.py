b23b0766925e2078996a8f5a3f74ec09f5d12eb3
Ivaylo Dionisov <ivaylo.dionisov@cern.ch>
2025-01-08 10:10:25 +0200

    Save to a legacy branch to prepare for large changes

---
+++ roostyling.py
@@ -0,0 +1,20 @@
+import ROOT
+
+def axes(*objects, 
+    axesTitleSize:    float = 0.04, 
+    XaxisOffset:      float = 1.25, 
+    YaxisOffset:      float = 1.25, 
+    centerAxesTitles: bool  = True
+):
+    for object in objects: # loop through all the objects
+        object.GetXaxis().CenterTitle(centerAxesTitles)
+        object.GetYaxis().CenterTitle(centerAxesTitles)
+
+        object.GetXaxis().SetTitleSize(axesTitleSize)
+        object.GetYaxis().SetTitleSize(axesTitleSize)
+
+        object.GetXaxis().SetTitleOffset(XaxisOffset)
+        object.GetYaxis().SetTitleOffset(YaxisOffset)
+
+        if hasattr(object, 'GetZaxis'):
+            object.GetZaxis().CenterTitle(centerAxesTitles)
